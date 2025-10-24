3a8cc6fed1387a9615dda2680991ec8e87480fb2
JuanCRM <jcros84@gmail.com>
2025-10-24 13:12:59 +0200

    Configuraciones

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('app', './assets/sb-admin-2.min.js')
+    .addEntry('sbadmin', './assets/sb-admin-2.min.js')
     
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
